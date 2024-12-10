d5d41e2d0b086e1399abde899c9e3a8cf6a39deb
Ruslanqaa <ruskiseljov1@gmail.com>
2024-12-10 22:26:31 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,31 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        # Добавляем название объекта в историю
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name, floors):
+        self.name = name
+        self.floors = floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+# Примеры использования
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)  # ['ЖК Эльбрус']
+
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация']
+
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация', 'ЖК Матрёшки']
+
+# Удаление объектов
+del h2  # ЖК Акация снесён, но он останется в истории
+del h3  # ЖК Матрёшки снесён, но он останется в истории
+
+print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация', 'ЖК Матрёшки']
+del h1  # ЖК Эльбрус снесён, но он останется в истории
